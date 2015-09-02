5ace1bcf31bede60c700cb28f2efdfb513edce72
Pedro Duarte <pedro.duarte@tecnico.ulisboa.pt>
2015-09-02 02:44:00 +0100

    Tests to check url consistency.

--- test.js
+++ test.js
@@ -1,7 +1,23 @@
 'use strict';
-var assert = require('assert');
+var assert          = require('assert');
 var googleStoreLink = require('./');
+var baseURL         = 'https://play.google.com/store/apps/details?id=';
+var id              = 'com.duolingo';
+var lang            = 'pt-PT';
 
+// Test link construction (no null parameters).
 it('should ', function () {
-	assert.strictEqual(googleStoreLink('unicorns'), 'unicorns & rainbows');
+	assert.strictEqual(
+		googleStoreLink(id), baseURL + id
+	);
+	assert.strictEqual(
+		googleStoreLink(id, lang), baseURL + id + '&hl=' + lang
+	);
+});
+
+// Test link construction (with null parameters).
+it('should ', function () {
+	assert.strictEqual(
+		googleStoreLink(id, null), baseURL + id
+	);
 });
